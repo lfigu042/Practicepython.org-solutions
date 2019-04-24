4c0e5dbe815a083e7ea921df037159db7b465607
Laura Figueroa <lfigu042@fiu.edu>
2019-04-24 12:55:25 -0400

    Update excercise_2.py

--- excercise_2.py
+++ excercise_2.py
@@ -3,3 +3,13 @@
 # If the number is a multiple of 4, print out a different message.
 # Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). 
 # If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+
+num = int(input("Enter num to check: "))
+check = int(input("Enter num to divide by : "))
+
+if(num%4 == 0):
+  print(" number is divisible by 4!")
+elif(num%check == 0):
+  print("The number divided evenly")
+else:
+  print("The division left a remainder")
